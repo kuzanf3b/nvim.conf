4cdd455bf85bde5aebbc255b56be4a54fd2ef367
fauzan-febrian <0differenthang@gmail.com>
2025-09-01 21:37:05 +0700

    Still broke for localhost

---
+++ lua/plugins/live-server.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "barrett-ruth/live-server.nvim",
+    build = "npm install -g live-server", -- install live-server kalau belum ada
+    config = function()
+      require("live-server").setup({
+        args = { "--browser=flatpak run app.zen_browser.zen" },
+      })
+
+      -- Keymaps
+      vim.keymap.set("n", "<leader>ls", "<cmd>LiveServerStart<cr>", { desc = "Start Live Server" })
+      vim.keymap.set("n", "<leader>lS", "<cmd>LiveServerStop<cr>", { desc = "Stop Live Server" })
+    end,
+  },
+}
