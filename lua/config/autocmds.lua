2d1d66ac824b89e433eaa27585fc3339fa53e715
Sabun Zenzen <kuzanSUSE@localhost.localdomain>
2025-09-07 07:45:04 +0700

    reformat config.h

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,15 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWritePre", {
+  pattern = "config.h",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
