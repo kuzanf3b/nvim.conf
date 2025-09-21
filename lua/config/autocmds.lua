b73a2553c41003d4f6c0f1c530c7e671606e3976
kuzanf3b <0differenthang@gmail.com>
2025-09-21 09:40:16 +0700

    Turn off Formater for DWM

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,12 +1,23 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufWritePre", {
-  pattern = "config.h",
-  callback = function()
-    vim.b.autoformat = false
+-- Disable autoformat untuk file penting dwm
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.h" },
+  callback = function(args)
+    local fname = vim.api.nvim_buf_get_name(args.buf)
+    if
+      fname:match("/home/kuzan/dwm%-dotfiles/dwm/config%.h$")
+      or fname:match("/home/kuzan/dwm%-dotfiles/dwm/config%.def%.h$")
+      or fname:match("/home/kuzan/dwm%-dotfiles/dwm/dwm%.c$")
+    then
+      -- Disable conform.nvim autoformat
+      vim.b[args.buf].disable_autoformat = true
+      vim.notify("Autoformat disabled for " .. vim.fn.fnamemodify(fname, ":t"))
+    end
   end,
 })
 
+-- Highlight text setelah yank
 autocmd("TextYankPost", {
   pattern = "*",
   callback = function()
