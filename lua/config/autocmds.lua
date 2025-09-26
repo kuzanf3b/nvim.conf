503149a1127b9668be3329e0962c5831d9e8df76
kuzanf3b <0differenthang@gmail.com>
2025-09-26 15:25:20 +0700

    Change ColorScheme TokyoOGNight

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,18 +1,17 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Disable autoformat untuk file penting dwm
-vim.api.nvim_create_autocmd("BufWritePre", {
+-- Disable autoformat khusus file DWM
+autocmd("BufWritePre", {
   pattern = { "*.c", "*.h" },
   callback = function(args)
     local fname = vim.api.nvim_buf_get_name(args.buf)
     if
-      fname:match("/home/kuzan/dwm%-dotfiles/dwm/config%.h$")
-      or fname:match("/home/kuzan/dwm%-dotfiles/dwm/config%.def%.h$")
-      or fname:match("/home/kuzan/dwm%-dotfiles/dwm/dwm%.c$")
+      fname:match("dwm%-dotfiles/dwm/config%.h$")
+      or fname:match("dwm%-dotfiles/dwm/config%.def%.h$")
+      or fname:match("dwm%-dotfiles/dwm/dwm%.c$")
     then
-      -- Disable conform.nvim autoformat
-      vim.b[args.buf].disable_autoformat = true
-      vim.notify("Autoformat disabled for " .. vim.fn.fnamemodify(fname, ":t"))
+      vim.b.autoformat = false -- ini yang dibaca oleh conform.nvim
+      vim.notify("🚫 Autoformat disabled for " .. vim.fn.fnamemodify(fname, ":t"))
     end
   end,
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,17 @@
 return {
   {
-    "shaunsingh/nord.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("nord")
+      vim.g.tokyonight_style = "night" -- opsi: storm, night, day
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      theme = "nord",
-    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = { theme = "tokyonight" },
   },
 }
