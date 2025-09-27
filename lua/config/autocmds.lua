e56e3fc97cd5e0719fe73fc38b5f96c9fbcce2ea
kuzanf3b <0differenthang@gmail.com>
2025-09-27 14:42:02 +0700

    Disable Format for suckless

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,17 +1,33 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Disable autoformat khusus file DWM
+-- Disable autoformat khusus file DWM, dmenu, slock, dwmblocks
 autocmd("BufWritePre", {
   pattern = { "*.c", "*.h" },
   callback = function(args)
     local fname = vim.api.nvim_buf_get_name(args.buf)
-    if
-      fname:match("dwm%-dotfiles/dwm/config%.h$")
-      or fname:match("dwm%-dotfiles/dwm/config%.def%.h$")
-      or fname:match("dwm%-dotfiles/dwm/dwm%.c$")
-    then
-      vim.b.autoformat = false -- ini yang dibaca oleh conform.nvim
-      vim.notify("🚫 Autoformat disabled for " .. vim.fn.fnamemodify(fname, ":t"))
+
+    -- Daftar regex untuk menonaktifkan autoformat
+    local disabled_files = {
+      "dwm%-dotfiles/dwm/config%.h$",
+      "dwm%-dotfiles/dwm/config%.def%.h$",
+      "dwm%-dotfiles/dwm/dwm%.c$",
+      "dwm%-dotfiles/dmenu/config%.h$",
+      "dwm%-dotfiles/dmenu/config%.def%.h$",
+      "dwm%-dotfiles/dmenu/dmenu%.c$",
+      "dwm%-dotfiles/slock/config%.h$",
+      "dwm%-dotfiles/slock/config%.def%.h$",
+      "dwm%-dotfiles/slock/slock%.c$",
+      "dwm%-dotfiles/dwmblocks/blocks%.h$",
+      "dwm%-dotfiles/dwmblocks/blocks%.def%.h$",
+      "dwm%-dotfiles/dwmblocks/dwmblocks%.c$",
+    }
+
+    for _, pattern in ipairs(disabled_files) do
+      if fname:match(pattern) then
+        vim.b.autoformat = false
+        vim.notify("🚫 Autoformat disabled for " .. vim.fn.fnamemodify(fname, ":t"))
+        break
+      end
     end
   end,
 })
