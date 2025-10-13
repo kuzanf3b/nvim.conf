a8a7cb96906bf3e0abc5bf32256bb120a86cf1d3
Zen <0differenthanG@gmail.com>
2025-10-13 22:08:23 +0700

    comeback neoscroll

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -20,7 +20,6 @@ return {
 		local opts = {
 			theme = "doom",
 			hide = {
-				-- this is taken care of by lualine
 				-- enabling this messes up the actual laststatus setting after loading a file
 				statusline = false,
 			},
